949e154482d6c243689a5ccab9439268255841f6
AlexMontgomeryC <azlodore@montgomerycollege.edu>
2026-05-05 12:53:30 -0400

    Update PrimeNumberMethodTest.java

--- PrimeNumberMethodTest.java
+++ PrimeNumberMethodTest.java
@@ -3,16 +3,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 /** This class will test PrimeNumberMethod.java class
  */ 
-/*
- * Class: CMSC201 CRN30111
- * Instructor: Grigoriy A. Grinberg
- * Description: (Tests length(), charAt(), and the overloaded methods indexOf and substring in String class)
- * Due: 05/11/26
- * I pledge that I have completed the programming assignment independently.
-   I have not copied the code from a student or any source.
-   I have not given my code to any student.
-   Print your Name here: Alexander Zlodorev
-*/
 class PrimeNumberMethodTest {
 	
 
